573070995645968932682ce0591aa3fa507a07cb
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2025-05-02 15:51:03 +0100

    2024 day 8

---
+++ 2024/day-08/part2.py
@@ -0,0 +1,42 @@
+import itertools
+
+with open('input.txt', 'r') as file:
+    lines = [line.strip() for line in file]
+
+antenna_map = [[c for c in line] for line in lines]
+antenna_types = set()
+for row in antenna_map:
+    for col in row:
+        if col != ".":
+            antenna_types.add(col)
+
+length = len(antenna_map)
+#antinode_map = [['.'] * length for _ in range(length)]
+antinode_map = [['.' for i in range(length)] for j in range(length)]
+
+for antenna_type in antenna_types:
+    antenna_locations = []
+    for row_index, row in enumerate(antenna_map):
+        for col_index, col in enumerate(row):
+            if col == antenna_type:
+                antenna_locations.append((row_index, col_index))
+    atenna_permutations = list(itertools.permutations(antenna_locations, 2))
+    print(antenna_locations)
+    for atenna_permutation in atenna_permutations:
+        inverse_vector = ((atenna_permutation[1][0] - atenna_permutation[0][0]) * -1, (atenna_permutation[1][1]-atenna_permutation[0][1]) * -1)
+        #antinode_location = [atenna_permutation[0][0] + inverse_vector[0], atenna_permutation[0][1] + inverse_vector[1]]
+        antinode_location = [atenna_permutation[0][0], atenna_permutation[0][1]]
+        while antinode_location[0] < length and antinode_location[0] >= 0 and antinode_location[1] < length and antinode_location[1] >= 0:
+            antinode_map[antinode_location[0]][antinode_location[1]] = "#"
+            antinode_location = [antinode_location[0] + inverse_vector[0], antinode_location[1] + inverse_vector[1]]
+
+antinode_count = 0
+for row in antinode_map:
+    for col in row:
+        if col != ".":
+            antinode_count += 1
+
+#print(antenna_types)
+for line in antinode_map:
+    print("".join(line))
+print(antinode_count)
